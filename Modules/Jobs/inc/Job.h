4a3e9864b6940aabc09d8744017925707f06e9cb
natecheadle_school <natecheadle@users.noreply.github.com>
2022-02-07 20:32:56 -0800

    Completed basic Jobs Base Class

---
+++ Modules/Jobs/inc/Job.h
@@ -0,0 +1,35 @@
+#include <chrono>
+#include <condition_variable>
+#include <future>
+#include <mutex>
+
+namespace nate::Modules::Jobs {
+
+    class Job {
+        std::future<void> m_Job;
+        std::atomic<bool> m_ShouldStop{false};
+        std::atomic<bool> m_ShouldPause{false};
+
+        std::mutex              m_YieldMutex;
+        std::condition_variable m_YieldCondition;
+
+      public:
+        Job() = default;
+        virtual ~Job();
+
+        void Start();
+        void Stop() { m_ShouldStop = true; }
+        void Pause() { m_ShouldPause = true; }
+        void Continue();
+
+        bool IsPaused() const { return m_ShouldPause; }
+        bool IsExecuting() const;
+
+        std::future_status Join(std::chrono::milliseconds timeout);
+
+      protected:
+        virtual void ExecuteJob() = 0;
+        void         Yield();
+        bool         ShouldStop() const { return m_ShouldStop; }
+    };
+} // namespace nate::Modules::Jobs
