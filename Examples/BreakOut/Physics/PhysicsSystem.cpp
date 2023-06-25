cc27e77809b0a3868d682f8d59234296f6a05cda
natecheadle <natecheadle@gmail.com>
2023-06-24 18:22:43 -0700

    Transfered current physics classes from other branch

---
+++ Examples/BreakOut/Physics/PhysicsSystem.cpp
@@ -0,0 +1,61 @@
+#include "PhysicsSystem.h"
+
+#include <LinearAlgebra/Vector2.hpp>
+
+using namespace std::chrono_literals;
+
+namespace nate::Modules::Physics
+{
+    PhysicsSystem::PhysicsSystem(Memory::PoolMemoryBlock<RigidBody2D>* pRigidBodyPool)
+        : ECS::System<RigidBody2D>(pRigidBodyPool)
+    {
+    }
+
+    void PhysicsSystem::Update(std::chrono::nanoseconds dt)
+    {
+        float del_t = static_cast<float>(dt / 1.0s);
+        auto& pool  = GetPool<RigidBody2D>();
+        m_MovingObjects.clear();
+        for (auto& body : pool)
+        {
+            body.Update(del_t);
+            if (body.Velocity() != Vector2<float>{0.0f, 0.0f})
+            {
+                m_MovingObjects.push_back(&body);
+                Vector2<float> pos = body.Position();
+                Vector2<float> vel = body.Velocity();
+                pos.x(pos.x() + vel.x());
+                pos.y(pos.y() + vel.y());
+
+                body.Position(pos);
+            }
+        }
+
+        for (auto& pBody : m_MovingObjects)
+        {
+            for (auto& body : pool)
+            {
+                if (&body == pBody)
+                    continue;
+
+                if (CheckCollision(*pBody, body))
+                {
+                    pBody->CollisionOccurred(body);
+                    body.CollisionOccurred(*pBody);
+                }
+            }
+        }
+    }
+
+    bool PhysicsSystem::CheckCollision(const RigidBody2D& one, const RigidBody2D& two)
+    {
+        // collision x-axis?
+        bool collisionX = one.Position()[0] + one.HitBox()[0] >= two.Position()[0] &&
+                          two.Position()[0] + two.HitBox()[0] >= one.Position()[0];
+        // collision y-axis?
+        bool collisionY = one.Position()[1] + one.HitBox()[1] >= two.Position()[1] &&
+                          two.Position()[1] + two.HitBox()[1] >= one.Position()[1];
+        // collision only if on both axes
+        return collisionX && collisionY;
+    }
+} // namespace nate::Modules::Physics
---
+++ Examples/BreakOut/Physics/RigidBody2D.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <LinearAlgebra/Vector2.hpp>
+
+#include <functional>
+
+namespace nate::Modules::Physics
+{
+    class PhysicsSystem;
+
+    class RigidBody2D
+    {
+        Vector2<float> m_Position;
+        Vector2<float> m_Velocity;
+        Vector2<float> m_HitBox;
+        bool           m_IsFixed{true};
+
+        std::function<void(const Vector2<float>&)>    m_OnPosChange;
+        std::function<void(const RigidBody2D& other)> m_OnCollision;
+
+        friend PhysicsSystem;
+
+      public:
+        void Update(float dt);
+
+        void AttachOnPosChange(std::function<void(const Vector2<float>&)>&& func) { m_OnPosChange = std::move(func); }
+        void AttachOnCollision(std::function<void(const RigidBody2D& other)>&& func)
+        {
+            m_OnCollision = std::move(func);
+        }
+        void ClearCallbacks();
+
+        void IsFixed(bool val) { m_IsFixed = val; }
+        bool IsFixed() const { return m_IsFixed; }
+
+        const Vector2<float>& Position() const { return m_Position; }
+        void                  Position(const Vector2<float>& val)
+        {
+            m_Position = val;
+            if (m_OnPosChange)
+                m_OnPosChange(m_Position);
+        }
+
+        const Vector2<float>& Velocity() const { return m_Velocity; }
+        void                  Velocity(const Vector2<float>& val) { m_Velocity = val; }
+
+        const Vector2<float>& HitBox() const { return m_HitBox; }
+        void                  HitBox(const Vector2<float>& val) { m_HitBox = val; }
+
+      private:
+        void CollisionOccurred(const RigidBody2D& other);
+    };
+} // namespace nate::Modules::Physics
