ae83fa56f358967a62705df12f95509497bc8748
natecheadle <natecheadle@gmail.com>
2023-09-04 16:23:12 -0700

    formatting update

--- Modules/ECS/IWorld.h
+++ Modules/ECS/IWorld.h
@@ -8,6 +8,7 @@ namespace Ignosi::Modules::ECS
     {
       public:
         virtual void                         Update(double dt)                          = 0;
+
         virtual const std::vector<EntityID>& AllEntities() const                        = 0;
         virtual const std::vector<EntityID>& GetEntitiesByTag(const Tag& tag) const     = 0;
         virtual IEntity*                     GetEntity(EntityID id)                     = 0;
