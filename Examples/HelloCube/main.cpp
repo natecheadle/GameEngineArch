46fef816b46d74918074be862d2ba352d7678636
natecheadle <natecheadle@gmail.com>
2024-05-18 13:46:19 -0700

    moved to app class

--- Examples/HelloCube/main.cpp
+++ Examples/HelloCube/main.cpp
@@ -1,68 +1,8 @@
-#include "Entity.hpp"
-#include "Objects/Mesh.h"
-#include "Shader/IShader.h"
-#include "VertexDataConfig.h"
-
-#include <IRenderer.h>
-#include <IWindow.h>
-#include <World.hpp>
-#include <fmt/format.h>
-
-#include <array>
-#include <memory>
-
-using namespace Ignosi::Libraries;
-using namespace std::chrono_literals;
-
-using TestWorld  = ECS::World<Renderer::Mesh>;
-using TestEntity = ECS::Entity<Renderer::Mesh>;
-
-const std::filesystem::path OutDir(OUT_DIR);
+#include "HelloCubeApp.h"
 
 int main(int argc, char** argv)
 {
-    TestWorld                           world;
-    Containers::PoolPointer<TestEntity> entity = world.CreateEntity();
+    Ignosi::Example::HelloCubeApp app;
 
-    std::unique_ptr<Renderer::IRenderer> pRendererUnique = Renderer::IRenderer::Create();
-    Renderer::IRenderer*                 pRenderer       = world.Register<Renderer::Mesh>(std::move(pRendererUnique));
-    Renderer::IWindow*                   pWindow         = pRenderer->AttachWindow(Renderer::IWindow::Create("test_window"));
-
-    Renderer::VertexDataConfig vertexData;
-    vertexData.PushBackConfig(Math::Vector3<float>());
-    std::array<float, 9> vertices{-0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f};
-
-    ECS::ResourceManager&                   resourceManager = world.Resources();
-    ECS::Resource<Renderer::IVertexBuffer>* pBuffer =
-        resourceManager.CreateResource("TriangleVertexes", pRenderer->CreateBuffer(vertexData, vertices));
-
-    ECS::Resource<Renderer::IShader>* pVertShader =
-        resourceManager.CreateResource("VertexShader", pRenderer->CreateShader(OutDir / "assets/vert_shader.vert"));
-
-    ECS::Resource<Renderer::IShader>* pFragShader =
-        resourceManager.CreateResource("FragmentShader", pRenderer->CreateShader(OutDir / "assets/frag_shader.frag"));
-    ECS::Resource<Renderer::IShaderProgram>* pShaderProgram =
-        resourceManager.CreateResource("ShaderProgram", pRenderer->CreateShaderProgram(pFragShader->get(), nullptr, pVertShader->get()));
-
-    entity->Set(Renderer::Mesh(pBuffer->get(), pShaderProgram->get(), nullptr));
-
-    unsigned int              update = 0;
-    std::chrono::milliseconds updatePeriod(1s / 60s);
-
-    while (!pWindow->ShouldClose())
-    {
-        if (update % 60 == 0)
-        {
-            fmt::print("Window.Name: {} Window.Size: {}\n", pWindow->Name(), pWindow->ActualWindowSize());
-        }
-
-        world.Update(updatePeriod);
-        update++;
-    }
-
-    // This is not explicitely necessary as the resource will be destroyed with the "World"
-    resourceManager.DestroyResource("TriangleVertexes");
-    resourceManager.DestroyResource("ShaderProgram");
-    resourceManager.DestroyResource("VertexShader");
-    resourceManager.DestroyResource("FragmentShader");
+    app.Run();
 }
